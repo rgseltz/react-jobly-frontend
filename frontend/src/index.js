323f859b96da60d4b84cb29da2d656414d22ae9a
ryan <ryanjgecelter@gmail.com>
2022-08-30 16:06:51 +0300

    imported bootstrap to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
