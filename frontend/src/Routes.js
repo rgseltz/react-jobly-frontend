b6424b5673cb44c70cade91a972421528785280b
ryan <ryanjgecelter@gmail.com>
2022-09-02 13:17:11 +0300

    created Jobs component, updated Routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,7 +4,7 @@ import Homepage from "./Homepage";
 import CompaniesList from "./CompaniesList";
 import CompanyDetail from "./CompanyDetail";
 import LoginForm from "./LoginForm";
-import JobsList from "./JobsList";
+import Jobs from "./Jobs";
 import SignupForm from "./SignupForm";
 import ProfileForm from './ProfileForm';
 // import CompanyConext from "./companyContext";
@@ -16,7 +16,7 @@ return (
         <Switch>
             <Route path={'/companies/:handle'}><CompanyDetail/></Route>
             <Route path={'/companies'}><CompaniesList/></Route>
-            <Route path={'/jobs'}><JobsList/></Route>
+            <Route path={'/jobs'}><Jobs/></Route>
             <Route path={'/profile-form'}><ProfileForm/></Route>
             <Route path={'/login'}><LoginForm/></Route>
             <Route path={'/register'}><SignupForm/></Route>
