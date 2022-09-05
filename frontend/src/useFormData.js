d98f4e9b14bfc355b5ac52e09cd469c4e8f28da9
ryan <ryanjgecelter@gmail.com>
2022-09-05 10:41:32 +0300

    custom-hook useFormData

---
+++ frontend/src/useFormData.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+const useFormData = (initialState) => {
+    const [formData, setFormData] = useState(initialState) 
+    const handleChange = (evt) => {
+        const [name, value] = evt.target;
+        setFormData(data => ({...data, [name] : value}));
+    }
+
+    const resetFormData = () => {
+        setFormData(initialState)
+    };
+    return [formData, handleChange, resetFormData];    
+}
