1b392f95cc5308340a9615c03c7d77d158533be4
ryan <ryanjgecelter@gmail.com>
2022-09-06 14:05:13 +0300

    bug fix

--- frontend/src/JobsList.js
+++ frontend/src/JobsList.js
@@ -4,7 +4,7 @@ const JobList = ({jobs}) => {
     return (
         <>
         <h1>Job List</h1>
-        {jobs.map(j => <JobCard key={j.id} company={j.compy} title={j.title} equity={j.equity} salary={j.salary}/>)}
+        {jobs.map(j => <JobCard key={j.id} id={j.id} company={j.compy} title={j.title} equity={j.equity} salary={j.salary}/>)}
         </>
     )
 }
